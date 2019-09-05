02c34a36caef5dee096dd53608fd96066cc6c4b5
Administrator <771968690@qq.com>
2019-09-05 16:34:31 +0800

    sunpeng

--- roes-be-service/src/main/java/com/rose/data/constant/SystemConstant.java
+++ roes-be-service/src/main/java/com/rose/data/constant/SystemConstant.java
@@ -2,7 +2,7 @@ package com.rose.data.constant;
 
 public interface SystemConstant {
     // session 活动最大间隔时间
-    int SESSION_MAX_INACTIVE_INTERVAL = 3 * 60;
+    int SESSION_MAX_INACTIVE_INTERVAL = 10 * 60;
 
     // session 用户登录验证码 key
     String SESSION_LOGIN_CODE_KEY = "loginCode";
